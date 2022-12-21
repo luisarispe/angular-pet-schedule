b4af9a3e16bae3d427729d1e738fe7573f71ac0a
luisarispe <luis.arispe1991@gmail.com>
2022-12-21 16:22:42 -0300

    cambiando url de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  base_url:
-    'http://petschedule-env.eba-kaa3bapr.us-east-1.elasticbeanstalk.com/',
+  base_url: 'https://api.petschedule.luisarispe.com/',
 };
