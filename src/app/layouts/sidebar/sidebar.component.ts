5557d6a911c960e1d0a68dfab5e8c83764e34ae8
luisarispe <luis.arispe1991@gmail.com>
2022-12-29 18:23:17 -0300

    agregando sub menu

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  hiddenSubM = false;
+  changeSubMenu() {
+    this.hiddenSubM = this.hiddenSubM ? false : true;
+    console.log(this.hiddenSubM);
+  }
 }
